6fa1a2abf8caadba7757f1d3f3376cf8af11b773
Ravi Chamarthy <ravi@chamarthy.me>
2017-11-15 12:14:57 -0800

    Add service groups and lifecycle

---
+++ service/group.go
@@ -0,0 +1,98 @@
+package service
+
+import (
+	"github.com/anuvu/dig"
+)
+
+type Group struct {
+	name      string
+	parent    *Group
+	container *dig.Container
+	ctx       *srvCtx
+}
+
+// NewGroup creates a new service group with the specified parent. If the parent is nil
+// this group is the root group.
+func NewGroup(name string, parent *Group) *Group {
+	var container *dig.Container
+	var ctx *srvCtx
+	if parent == nil {
+		container = dig.New()
+		ctx = newContext()
+	} else {
+		container = dig.NewWithParent(parent.container)
+		ctx = parent.ctx
+	}
+	grp := &Group{
+		name:      name,
+		parent:    parent,
+		container: container,
+		ctx:       ctx,
+	}
+
+	// Provide the context if we are the root group
+	if grp.parent == nil {
+		grp.container.Provide(func() Context {
+			return grp.ctx
+		})
+	}
+	return grp
+}
+
+// AddService adds a new service constructor to the service group.
+func (g *Group) AddService(ctr interface{}) error {
+	// add the service constructor to the container
+	return g.container.Provide(ctr)
+}
+
+// Configure calls the configure hooks on all services registered for configuration.
+func (g *Group) Configure() error {
+	for _, h := range g.ctx.hooks {
+		if h.ConfigHook != nil {
+			if err := g.container.Invoke(h.ConfigHook); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// Start calls the start hooks on all services registered for startup.
+// If an error occurs on any hook, subsequent start calls are abandoned
+// and a best effort stop is initiated.
+func (g *Group) Start() error {
+	for _, h := range g.ctx.hooks {
+		if h.StartHook != nil {
+			if err := g.container.Invoke(h.StartHook); err != nil {
+				defer g.Stop()
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// Stop calls the stop hooks on all services registered for shutdown.
+func (g *Group) Stop() error {
+	// Invoke the stop hooks in the reverse dependency order
+	for i := len(g.ctx.hooks); i > 0; {
+		i--
+		h := g.ctx.hooks[i]
+		if h.StopHook != nil {
+			if err := g.container.Invoke(h.StopHook); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// IsHealthy returns true if all services health hooks return true else false
+func (g *Group) IsHealthy() bool {
+	for _, h := range g.ctx.hooks {
+		if h.HealthHook != nil && !h.HealthHook() {
+			return false
+		}
+	}
+	return true
+}
---
+++ service/group_test.go
@@ -0,0 +1,153 @@
+package service
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+type svc struct {
+	startCalled     bool
+	stopCalled      bool
+	configureCalled bool
+}
+
+type svcWithHooks svc
+
+func newSvcWithHooks(ctx Context) *svcWithHooks {
+	s := &svcWithHooks{}
+	ctx.AddHook(&Lifecycle{
+		ConfigHook: func(svc *svcWithHooks) { svc.configureCalled = true },
+		StartHook:  func(svc *svcWithHooks) { svc.startCalled = true },
+		StopHook:   func(svc *svcWithHooks) { svc.stopCalled = true },
+		HealthHook: func() bool { return s.startCalled && !s.stopCalled },
+	})
+	return s
+}
+
+type svcWithErrors svc
+
+func newSvcWithErrors(ctx Context) *svcWithErrors {
+	s := &svcWithErrors{}
+	ctx.AddHook(&Lifecycle{
+		ConfigHook: func(svc *svcWithErrors) error {
+			svc.configureCalled = true
+			return fmt.Errorf("config error")
+		},
+		StartHook: func(svc *svcWithErrors) error {
+			svc.startCalled = true
+			return fmt.Errorf("start error")
+		},
+		StopHook: func(svc *svcWithErrors) error {
+			svc.stopCalled = true
+			return fmt.Errorf("stop error")
+		},
+		HealthHook: func() bool { return s.startCalled && !s.stopCalled },
+	})
+	return s
+}
+
+func TestGroup(t *testing.T) {
+	Convey("After we create a group", t, func() {
+		grp := NewGroup("base", nil)
+		So(grp, ShouldNotBeNil)
+		So(grp.parent, ShouldBeNil)
+		So(grp.ctx, ShouldNotBeNil)
+
+		Convey("we should be able to add a service with no hooks", func() {
+			s := &svc{}
+			So(grp.AddService(func(ctx Context) *svc { return s }), ShouldBeNil)
+			So(grp.Configure(), ShouldBeNil)
+			So(grp.Start(), ShouldBeNil)
+			So(grp.Stop(), ShouldBeNil)
+
+			// Assert that none of the hooks are called
+			So(s.configureCalled, ShouldBeFalse)
+			So(s.stopCalled, ShouldBeFalse)
+			So(s.startCalled, ShouldBeFalse)
+		})
+
+		Convey("we should be able to add service with hooks", func() {
+			s := newSvcWithHooks(grp.ctx)
+			So(grp.AddService(func(ctx Context) *svcWithHooks {
+				return s
+			}), ShouldBeNil)
+			So(grp.IsHealthy(), ShouldBeFalse)
+
+			Convey("we should be able to configure the group", func() {
+				So(grp.Configure(), ShouldBeNil)
+				So(s.configureCalled, ShouldBeTrue)
+				So(grp.IsHealthy(), ShouldBeFalse)
+			})
+			Convey("we should be able to start the group", func() {
+				So(grp.Start(), ShouldBeNil)
+				So(s.startCalled, ShouldBeTrue)
+				So(grp.IsHealthy(), ShouldBeTrue)
+			})
+			Convey("we should be able to stop the group", func() {
+				So(grp.Stop(), ShouldBeNil)
+				So(s.stopCalled, ShouldBeTrue)
+				So(grp.IsHealthy(), ShouldBeFalse)
+			})
+		})
+
+		Convey("check service with errors", func() {
+			s := newSvcWithErrors(grp.ctx)
+			So(grp.AddService(func(ctx Context) *svcWithErrors {
+				return s
+			}), ShouldBeNil)
+			So(grp.IsHealthy(), ShouldBeFalse)
+			Convey("configure the group should be error", func() {
+				So(grp.Configure(), ShouldNotBeNil)
+				So(s.configureCalled, ShouldBeTrue)
+				So(grp.IsHealthy(), ShouldBeFalse)
+			})
+			Convey("start should be error", func() {
+				So(grp.Start(), ShouldNotBeNil)
+				So(s.startCalled, ShouldBeTrue)
+				So(s.stopCalled, ShouldBeTrue)
+				So(grp.IsHealthy(), ShouldBeFalse)
+			})
+			Convey("stop should be error", func() {
+				So(grp.Stop(), ShouldNotBeNil)
+				So(s.stopCalled, ShouldBeTrue)
+				So(grp.IsHealthy(), ShouldBeFalse)
+			})
+		})
+	})
+}
+
+func TestGroupHierarchy(t *testing.T) {
+	Convey("Create the root group", t, func() {
+		root := NewGroup("root", nil)
+		So(root, ShouldNotBeNil)
+		Convey("create a sub group", func() {
+			grp := NewGroup("test", root)
+			So(grp, ShouldNotBeNil)
+			Convey("we should be able to add service with hooks", func() {
+				s := newSvcWithHooks(grp.ctx)
+				So(grp.AddService(func(ctx Context) *svcWithHooks {
+					return s
+				}), ShouldBeNil)
+				So(grp.IsHealthy(), ShouldBeFalse)
+
+				Convey("we should be able to configure the group", func() {
+					So(grp.Configure(), ShouldBeNil)
+					So(s.configureCalled, ShouldBeTrue)
+					So(grp.IsHealthy(), ShouldBeFalse)
+				})
+				Convey("we should be able to start the group", func() {
+					So(grp.Start(), ShouldBeNil)
+					So(s.startCalled, ShouldBeTrue)
+					So(grp.IsHealthy(), ShouldBeTrue)
+				})
+				Convey("we should be able to stop the group", func() {
+					So(grp.Stop(), ShouldBeNil)
+					So(s.stopCalled, ShouldBeTrue)
+					So(grp.IsHealthy(), ShouldBeFalse)
+				})
+			})
+		})
+	})
+}
